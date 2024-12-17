a01416412f55c0193175365a741f882c1a0a8bdc
Alexander Noriega <alexander.noriega@qubika.com>
2024-12-17 16:10:13 -0300

    feat: Add webhook handler.

---
+++ webhook.js
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default async function handler(req, res) {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
+  } else {
+    res.sendStatus(403);
+  }
+}
+
