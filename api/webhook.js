905c8ade12cd4d11b9e1b274baf75af6ae8e7622
Alexander Noriega <alexander.noriega@qubika.com>
2024-12-17 16:20:24 -0300

    Fix.

--- api/webhook.js
+++ api/webhook.js
@@ -6,12 +6,13 @@ export default async function handler(req, res) {
   const challenge = req.query["hub.challenge"];
 
   // check the mode and token sent are correct
-  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+  if (mode === "subscribe" && token === process.env.WEBHOOK_VERIFY_TOKEN) {
     // respond with 200 OK and challenge token from the request
-    res.status(200).json(challenge);
+    res.status(200).send(challenge);
     console.log("Webhook verified successfully!");
   } else {
     res.status(403).end();
   }
 }
 
+// https://qubika-whatsapp-bot-server.vercel.app/api/webhook?hub.mode=subscribe&hub.verify_token=%22qubika1234%22&hub.challenge=5678
