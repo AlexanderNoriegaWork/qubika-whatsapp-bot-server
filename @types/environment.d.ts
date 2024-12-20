8aee93940918a83b098ec317e9d45b4a9c87da7c
Alexander Noriega <alexander.noriega@qubika.com>
2024-12-20 14:15:57 -0300

    Add MAVENAGI env vars.

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -3,5 +3,9 @@ declare namespace NodeJS {
     WHATSAPP_API_ACCESS_TOKEN: string;
     WHATSAPP_API_BASE_URL: string;
     WHATSAPP_BOT_PHONE_NUMBER_ID: string;
+    MAVENAGI_APP_SECRET: string;
+    MAVENAGI_AGENT_ID: string;
+    MAVENAGI_ORGANIZATION_ID: string;
+    MAVENAGI_APP_ID: string;
   }
 }
