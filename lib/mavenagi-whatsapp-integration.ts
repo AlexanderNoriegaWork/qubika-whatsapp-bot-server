61afe76327c519fe473df523992dbd0a20852177
Alexander Noriega <alexander.noriega@qubika.com>
2025-01-23 12:29:38 -0300

    doc: Hack.

--- lib/mavenagi-whatsapp-integration.ts
+++ lib/mavenagi-whatsapp-integration.ts
@@ -21,6 +21,22 @@ export const handleChatMessage = async (
   const firstEntry = wppReq.entry[0];
   const firstMessage = firstEntry.changes[0].value.messages[0];
   console.log(`${LOG_CTX} incomingMessage`, firstMessage.text);
+
+  // HACK: The `.from.replace()` below is because the list of Allowed
+  // Phone Numbers used during the facebook app's development has them
+  // in a format (541115-...) that differs from the one in
+  // the actual messages received from (at least some of) our
+  // actual phones (54911-...).
+  //
+  // So this `.from.replace()` makes the incoming phone number data match
+  // their respective entries on the Allowed Phone Numbers list.
+  // (It only solves it for AR phones, obviously.)
+  //
+  // TODO: Either figure out a non-hacky normalization solution,
+  // or remove this, when/if you upgrade to a full business app
+  // that can phone any number, ie. isn't restricted by an Allowed Numbers list.
+  //
+  // (Already tried libphonenumber-js 3rd-party lib. Didn't cut it.)
   const recipientId: WhatsAppPhoneID = firstMessage.from.replace(
     /^54911/,
     "541115",
--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -14,22 +14,6 @@ export const replyTo = async (
   outgoing: string,
 ): Promise<AxiosResponse<any, any>> => {
   const accessToken = WHATSAPP_API_ACCESS_TOKEN;
-
-  // HACK: The `.from.replace()` below is because the list of Allowed
-  // Phone Numbers used during the facebook app's development has them
-  // in a format (541115-...) that differs from the one in
-  // the actual messages received from (at least some of) our
-  // actual phones (54911-...).
-  //
-  // So this `.from.replace()` makes the incoming phone number data match
-  // their respective entries on the Allowed Phone Numbers list.
-  // (It only solves it for AR phones, obviously.)
-  //
-  // TODO: Either figure out a non-hacky normalization solution,
-  // or remove this, when/if you upgrade to a full business app
-  // that can phone any number, ie. isn't restricted by an Allowed Numbers list.
-  //
-  // (Already tried libphonenumber-js 3rd-party lib. Didn't cut it.)
   const senderPhoneNumberId: WhatsAppPhoneID = WHATSAPP_BOT_PHONE_NUMBER_ID;
   const url = `${WHATSAPP_API_BASE_URL}/${senderPhoneNumberId}/messages`;
   const data = {
